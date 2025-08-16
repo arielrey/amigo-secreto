b1a61b53ff97ed549c275d9267788cdb1f0bf521
Ariel Rey <reynacho163@gmail.com>
2025-08-16 11:48:37 -0300

    Create agregarAmigo function

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres
+let amigos = [];
+
+// Implementa una función para agregar amigos
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    if(amigo == ""){
+        alert("El capo esta vacio, debe agregar un nombre.");
+    }else{
+        amigos.push(amigo);
+        limpiarCampoNombre();
+        console.log(amigos);
+    }
+}
+
+// Limpia el campo del nombre
+function limpiarCampoNombre(){
+    document.getElementById("amigo").value = "";
+}
+
+
+
